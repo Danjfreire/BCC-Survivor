71068d0c6b3096c4dfb9abbc0d4dd4ba400180ba
raylison100 <Ray99291072*>
2017-11-27 16:05:10 -0300

    Junção de partes
    Junção de partes

---
+++ app/src/main/java/com/ufrpe/bccsurvivor/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.ufrpe.bccsurvivor;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Raylison-WindowsN on 27/11/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
