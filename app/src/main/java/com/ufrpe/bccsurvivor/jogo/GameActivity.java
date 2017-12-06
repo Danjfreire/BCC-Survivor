826216b5bd2223d4675553072c8c31f8dd2d4136
Daniel Freire <danjfreire@gmail.com>
2017-12-06 08:27:23 -0200

    update apresentacao

--- app/src/main/java/com/ufrpe/bccsurvivor/jogo/GameActivity.java
+++ app/src/main/java/com/ufrpe/bccsurvivor/jogo/GameActivity.java
@@ -1,15 +1,35 @@
 package com.ufrpe.bccsurvivor.jogo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ufrpe.bccsurvivor.R;
 
 public class GameActivity extends AppCompatActivity {
 
+    TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        if(savedInstanceState!=null){
+            tv = (TextView)findViewById(R.id.disciplinajogo);
+            tv.setText(savedInstanceState.getString("disciplina"));
+        }
+
+        Button btn = (Button)findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(GameActivity.this,"selecionou alternativa A",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
