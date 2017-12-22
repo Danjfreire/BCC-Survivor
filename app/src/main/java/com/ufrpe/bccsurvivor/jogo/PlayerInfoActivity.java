0f3d0717f75f01dc2afe3ff91805d2666efbfe2b
Daniel Freire <danjfreire@gmail.com>
2017-12-22 15:29:00 -0200

    PlayerInfo

--- app/src/main/java/com/ufrpe/bccsurvivor/jogo/PlayerInfoActivity.java
+++ app/src/main/java/com/ufrpe/bccsurvivor/jogo/PlayerInfoActivity.java
@@ -1,15 +1,26 @@
 package com.ufrpe.bccsurvivor.jogo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.ufrpe.bccsurvivor.R;
 
 public class PlayerInfoActivity extends AppCompatActivity {
 
+    private TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_info);
+
+        if(savedInstanceState != null){
+            Intent i = getIntent();
+            Player p = (Player) i.getParcelableExtra("player");
+            tv = (TextView) findViewById(R.id.tv_player);
+            tv.setText(p.getNickname());
+        }
     }
 }
--- app/src/main/java/com/ufrpe/bccsurvivor/jogo/RankingActivity.java
+++ app/src/main/java/com/ufrpe/bccsurvivor/jogo/RankingActivity.java
@@ -4,6 +4,7 @@ import android.app.FragmentManager;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.ufrpe.bccsurvivor.R;
 
@@ -33,6 +34,7 @@ public class RankingActivity extends AppCompatActivity implements RankingFragmen
     @Override
     public void clicouNoPlayer(Player p) {
         Intent it = new Intent(this, PlayerInfoActivity.class);
+        it.putExtra("player",p);
         startActivity(it);
     }
 }
